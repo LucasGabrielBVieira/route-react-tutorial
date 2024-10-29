9e0673ea2db637bfb59a7ad083a00d792a793616
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-10-29 15:35:54 -0300

    Reading URL Params

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 function Invoices() {
@@ -26,6 +26,7 @@ function Invoices() {
             </Link>
           ))}
         </nav>
+        <Outlet/>
       </div>
     </>
   );
