fb9e6c853fa905db34afa555dfe4336a69c1f909
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-10-29 14:33:20 -0300

    Listing Invoices

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,13 +1,34 @@
-
+import { Link } from "react-router-dom";
+import { getInvoices } from "../../data";
 
 function Invoices() {
+  const invoices = getInvoices();
 
-    return(
-        <main style={{padding: "1rem 0"}}>
-            <h2>Invoices</h2>
-        </main>
-    );
-
+  return (
+    <>
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Invoices</h2>
+      </main>
+      <div style={{ display: "flex" }}>
+        <nav
+          style={{
+            borderRight: "solid 1px",
+            padding: "1rem",
+          }}
+        >
+          {invoices.map((invoice) => (
+            <Link
+              style={{ display: "block", margin: "1rem 0" }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default Invoices
+export default Invoices;
